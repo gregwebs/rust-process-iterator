81bd4c7a9618cc288669789183b52eb824e859cc
Greg Weber <greg@gregweber.info>
2017-03-06 15:50:58 -0800

    move from main function to tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,25 +1,26 @@
-#![allow(dead_code)]
-
 extern crate process_iterator;
 use process_iterator::{process_as_iterator, output, Output};
 
 use std::fs::File;
+use std::iter::{Iterator};
 use std::io::prelude::*;
 
-pub fn main(){
-//   let (tx, rx) = mpsc::channel();
-    let f = File::open("unsorted.txt").unwrap();
+#[test]
+fn hello_world() {
+    let f = File::open("tests/unsorted.txt").unwrap();
 
     // let some_in: Option<File> = None;
     let mut input_stream = 
             process_as_iterator(output().stderr(Output::Parent),
-                Some(f)
-             ,  streams::sort_cmd(&vec![])
-             ).expect("process iterator failure");
+                 Some(f)
+              ,  streams::sort_cmd(&vec![])
+              ).expect("process iterator failure");
 
     let mut output = String::new();
     input_stream.read_to_string(&mut output).unwrap();
-    print!("{}", &output);
+
+    let nums: Vec<i32> = output.lines().map(|line| line.parse().unwrap()).collect();
+    assert_eq!(vec![1,2,4,8], nums);
 }
 
 mod streams {
