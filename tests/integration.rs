cc22ddf5077c303d38abe0fd5dedc2f000c99cf3
Greg Weber <greg@gregweber.info>
2017-03-06 21:41:16 -0800

    add consumer test case

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,15 +1,14 @@
 extern crate process_iterator;
-use process_iterator::{process_as_iterator, output, Output};
+use process_iterator::{process_as_iterator, process_as_consumer, output, Output};
 
 use std::fs::File;
 use std::iter::{Iterator};
 use std::io::prelude::*;
 
 #[test]
-fn hello_world() {
-    let f = File::open("tests/unsorted.txt").unwrap();
+fn sort_iterator() {
+    let f = File::open("tests/files/unsorted.txt").unwrap();
 
-    // let some_in: Option<File> = None;
     let mut input_stream = 
             process_as_iterator(output().stderr(Output::Parent),
                  Some(f)
@@ -23,6 +22,24 @@ fn hello_world() {
     assert_eq!(vec![1,2,4,8], nums);
 }
 
+#[test]
+fn zip_consumer() {
+    let f = File::open("tests/files/unsorted.txt").unwrap();
+    let output_file = File::create("tests/files/output.gz").unwrap();
+
+    let output_status =
+            process_as_consumer(
+                output().stderr(Output::Parent)
+                        .stdout(Output::ToFd(output_file))
+              ,  f
+              ,  streams::gzipped_cmd()
+              ).expect("process iterator failure");
+
+    if !output_status.success() {
+        panic!("non-zero exit code {:?}", output_status.code());
+    }
+}
+
 mod streams {
     pub fn sort_cmd(columns: &Vec<(i32, &str)>) -> (String, Vec<String>) {
         let mut cmd: Vec<String> = Vec::new();
@@ -32,5 +49,9 @@ mod streams {
             cmd.push(format!("--key={0},{0}{1}", n + 1, order));
         }
         ("sort".to_owned(), cmd)
+    }
+
+    pub fn gzipped_cmd() -> (String, Vec<String>) {
+        ("/bin/gzip".to_owned(), Vec::new())
     }
 }
